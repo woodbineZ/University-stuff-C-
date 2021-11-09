82b8b820c5144f7324b06b2d6822f72dde7594c7
woodbineZ <72511757+woodbineZ@users.noreply.github.com>
2021-11-09 18:50:56 +0100

    Add files via upload

---
+++ MakuchJakub/Makuch/Makuch/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makuch
+{
+    internal class Rectangle : Shape
+    {
+        private double sideA;
+        private double sideB;
+        public double _sideA
+        {
+            get
+            {
+                return _sideA;
+            }
+            set
+            {
+                if (value > 0)
+                    _sideA = value;
+                else
+                    _sideA = 0;
+            }
+        }
+        public double _sideB
+        {
+            get
+            {
+                return _sideB;
+            }
+            set
+            {
+                if (value > 0)
+                    _sideB = value;
+                else
+                    _sideB = 0;
+            }
+        }
+        public Rectangle(double _sideA, double _sideB) // konstruktor klasy Rectangle
+        {
+            Name = "Prostokat";
+            sideA = _sideA;
+            sideB = _sideB;
+        }
+        public override double GetArea() //Metoda abstrakcyjna nadpisujaca sposob wywolania metody GetArea dla prostokata
+        {
+            return sideA * sideB;
+        }
+    }
+}
